01e1875551e61649869ec282067f2934eca442d6
albaqueroh <andres.baquero6@gmail.com>
2024-12-30 15:18:31 -0500

    feat: agregados test unitarios para el listado de seguidores por usuario

--- src/main/java/com/mercadolibre/sprint1/service/impl/UserServiceImpl.java
+++ src/main/java/com/mercadolibre/sprint1/service/impl/UserServiceImpl.java
@@ -1,20 +1,26 @@
 package com.mercadolibre.sprint1.service.impl;
 
 import java.time.LocalDate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
-import com.mercadolibre.sprint1.dto.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mercadolibre.sprint1.dto.ProductDto;
+import com.mercadolibre.sprint1.dto.UserDto;
 import com.mercadolibre.sprint1.dto.response.FollowedListByUserDto;
+import com.mercadolibre.sprint1.dto.response.FollowersListByUserDto;
 import com.mercadolibre.sprint1.dto.response.MostFollowedUserDto;
 import com.mercadolibre.sprint1.dto.response.UserPromosAverageDto;
 import com.mercadolibre.sprint1.dto.util.MostFollowersPostDto;
 import com.mercadolibre.sprint1.entity.Post;
+import com.mercadolibre.sprint1.entity.User;
 import com.mercadolibre.sprint1.entity.UserFollower;
 import com.mercadolibre.sprint1.exception.BadRequestException;
-
-import com.mercadolibre.sprint1.dto.response.FollowersListByUserDto;
-import com.mercadolibre.sprint1.entity.User;
 import com.mercadolibre.sprint1.exception.NoContentException;
 import com.mercadolibre.sprint1.exception.NotFoundException;
 import com.mercadolibre.sprint1.exception.PromoSellersNotFoundException;
@@ -23,8 +29,6 @@ import com.mercadolibre.sprint1.repository.impl.UserFollowerRepositoryImpl;
 import com.mercadolibre.sprint1.repository.impl.UserRepositoryImpl;
 import com.mercadolibre.sprint1.service.IUserService;
 import com.mercadolibre.sprint1.utils.Order;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements IUserService {
@@ -47,8 +51,8 @@ public class UserServiceImpl implements IUserService {
                 .map(f -> findUserById(f.getUserFollower()))
                 .map(u -> new UserDto(u.getId(), u.getName()))
                 .toList();
-        if(order != null){
-            followers = orderUserDtoList(followers,order);
+        if (order != null) {
+            followers = orderUserDtoList(followers, order);
         }
         FollowersListByUserDto res = new FollowersListByUserDto();
         res.setId(user.getId());
@@ -60,7 +64,7 @@ public class UserServiceImpl implements IUserService {
     @Override
     public boolean followUser(int userId, int userIdToFollow) {
         User findSeller = findUserById(userIdToFollow);
-        if(findSeller.isSeller()){
+        if (findSeller.isSeller()) {
             UserFollower userFollower = new UserFollower();
             userFollower.setUserFollowed(userIdToFollow);
             userFollower.setUserFollower(userId);
@@ -79,8 +83,8 @@ public class UserServiceImpl implements IUserService {
                 .map(f -> findUserById(f.getUserFollowed()))
                 .map(u -> new UserDto(u.getId(), u.getName()))
                 .toList();
-        if(order != null){
-            followed = orderUserDtoList(followed,order);
+        if (order != null) {
+            followed = orderUserDtoList(followed, order);
         }
         FollowedListByUserDto res = new FollowedListByUserDto();
         res.setId(user.getId());
@@ -103,42 +107,36 @@ public class UserServiceImpl implements IUserService {
     public List<UserPromosAverageDto> findUserPromoAverage() {
         Map<Integer, UserPromosAverageDto> promosAverages = new HashMap<>();
         List<Post> posts = postRepository.findAll();
-        if(posts.isEmpty()) throw new PromoSellersNotFoundException("No se encontraron vendedores con promociones activas");
-        for(Post post : posts){
-            if(post.getDiscount() <= 0) continue;
-            try{
+        if (posts.isEmpty())
+            throw new PromoSellersNotFoundException("No se encontraron vendedores con promociones activas");
+        for (Post post : posts) {
+            if (post.getDiscount() <= 0)
+                continue;
+            try {
                 User user = findUserById(post.getUserId());
                 UserPromosAverageDto userPromoDto = promosAverages.get(post.getUserId());
-                if(userPromoDto == null){
-                    promosAverages.put(post.getUserId(), new UserPromosAverageDto(user.getId(), user.getName(), post.getDiscount(), 1));
-                }else{
+                if (userPromoDto == null) {
+                    promosAverages.put(post.getUserId(),
+                            new UserPromosAverageDto(user.getId(), user.getName(), post.getDiscount(), 1));
+                } else {
                     userPromoDto.setPromoAverage(userPromoDto.getPromoAverage() + post.getDiscount());
                     userPromoDto.setCantPost(userPromoDto.getCantPost() + 1);
                 }
-            }catch (NotFoundException e){}
+            } catch (NotFoundException e) {
+            }
         }
 
-        if(promosAverages.isEmpty()) throw new PromoSellersNotFoundException("No se encontraron vendedores con promociones activas");
+        if (promosAverages.isEmpty())
+            throw new PromoSellersNotFoundException("No se encontraron vendedores con promociones activas");
         List<UserPromosAverageDto> userAveragesData = new ArrayList<>(promosAverages.values());
-        for(UserPromosAverageDto uad : userAveragesData){
-            uad.setPromoAverage(uad.getPromoAverage()/uad.getCantPost());
+        for (UserPromosAverageDto uad : userAveragesData) {
+            uad.setPromoAverage(uad.getPromoAverage() / uad.getCantPost());
         }
         return userAveragesData;
     }
 
-    private List<UserDto> orderUserDtoList(List<UserDto> userDtoList, String order){
-            return userDtoList.stream().sorted((f1, f2) -> {
-                if (order.equalsIgnoreCase(String.valueOf(Order.NAME_ASC))) {
-                    return f1.getName().compareTo(f2.getName());
-                } else if (order.equalsIgnoreCase(String.valueOf(Order.NAME_DESC))) {
-                    return f2.getName().compareTo(f1.getName());
-                } else {
-                    throw new BadRequestException("El orden requerido no es valido.");
-                }
-            }).toList();
-    }
-
-    public MostFollowedUserDto findMostFollowedUser(){
+    @Override
+    public MostFollowedUserDto findMostFollowedUser() {
 
         Map<Integer, Long> followerCounts = userFollowerRepositoryImpl.findAll().stream()
                 .collect(Collectors.groupingBy(UserFollower::getUserFollowed, Collectors.counting()));
@@ -161,17 +159,34 @@ public class UserServiceImpl implements IUserService {
                         post.getId(),
                         post.getUserId(),
                         post.getDate(),
-                        new ProductDto(post.getProduct().getId(), post.getProduct().getName(), post.getProduct().getType(), post.getProduct().getBrand(), post.getProduct().getColor(), post.getProduct().getNotes()),
+                        new ProductDto(post.getProduct().getId(), post.getProduct().getName(),
+                                post.getProduct().getType(), post.getProduct().getBrand(), post.getProduct().getColor(),
+                                post.getProduct().getNotes()),
                         post.getCategory(),
-                        post.getPrice()
-                ))
+                        post.getPrice()))
                 .collect(Collectors.toList());
 
         if (activePosts.isEmpty()) {
             throw new NoContentException("El usuario más seguido no tiene publicaciones activas.");
         }
 
-        return new MostFollowedUserDto(mostFollowedUser.getId(), mostFollowedUser.getName(), mostFollowers, activePosts);
+        return new MostFollowedUserDto(mostFollowedUser.getId(), mostFollowedUser.getName(), mostFollowers,
+                activePosts);
+    }
+
+    private List<UserDto> orderUserDtoList(List<UserDto> userDtoList, String order) {
+        if (!(order.equalsIgnoreCase(String.valueOf(Order.NAME_ASC))
+                || order.equalsIgnoreCase(String.valueOf(Order.NAME_DESC)))) {
+            throw new BadRequestException("El orden requerido no es valido.");
+        }
+
+        return userDtoList.stream().sorted((f1, f2) -> {
+            if (order.equalsIgnoreCase(String.valueOf(Order.NAME_ASC))) {
+                return f1.getName().compareTo(f2.getName());
+            }
+
+            return f2.getName().compareTo(f1.getName());
+        }).toList();
     }
 
 }
--- src/test/java/com/mercadolibre/sprint1/service/unit/UserServiceTest.java
+++ src/test/java/com/mercadolibre/sprint1/service/unit/UserServiceTest.java
@@ -1,29 +1,99 @@
 package com.mercadolibre.sprint1.service.unit;
 
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import java.util.Comparator;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.mercadolibre.sprint1.dto.response.FollowersListByUserDto;
 import com.mercadolibre.sprint1.entity.Post;
-import com.mercadolibre.sprint1.entity.User;
-import com.mercadolibre.sprint1.entity.UserFollower;
+import com.mercadolibre.sprint1.exception.BadRequestException;
 import com.mercadolibre.sprint1.repository.IRepository;
+import com.mercadolibre.sprint1.repository.impl.UserFollowerRepositoryImpl;
+import com.mercadolibre.sprint1.repository.impl.UserRepositoryImpl;
 import com.mercadolibre.sprint1.service.impl.UserServiceImpl;
+
+import util.TestUtilGenerator;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
     @Mock
-    IRepository<User> userRepository;
+    UserRepositoryImpl userRepository;
 
     @Mock
-    IRepository<UserFollower> userFollowerRepository;
+    UserFollowerRepositoryImpl userFollowerRepository;
 
     @Mock
     IRepository<Post> postRepository;
 
     @InjectMocks
     UserServiceImpl userService;
+
+    @BeforeEach()
+    public void initialize() {
+        when(userFollowerRepository.findAll()).thenReturn(TestUtilGenerator.generateFollowers());
+        when(userRepository.findAll()).thenReturn(TestUtilGenerator.generateUsers());
+    }
+
+    @Test
+    @DisplayName("Cuándo el orden enviado es NAME_ASC, debe listar todos los seguidores ordenados por nombre de forma ascendente")
+    public void whenOrderSendedIsNameAscShouldListFollowersSortedByNameDesc() {
+        // arrange
+        int userId = 2;
+
+        // act
+        FollowersListByUserDto res = userService.findAllFollowersByUser(userId, "NAME_ASC");
+
+        // assert
+        assertNotNull(res);
+        List<String> names = res.getFollowers()
+                .stream()
+                .map(u -> u.getName())
+                .toList();
+
+        assertIterableEquals(names.stream().sorted(Comparator.naturalOrder()).toList(), names);
+    }
+
+    @Test
+    @DisplayName("Cuándo el orden enviado es NAME_DESC, debe listar todos los seguidores ordenados por nombre de forma descendente")
+    public void whenOrderSendedIsNameDescShouldListFollowersSortedByNameAsc() {
+        // arrange
+        int userId = 2;
+
+        // act
+        FollowersListByUserDto res = userService.findAllFollowersByUser(userId, "NAME_DESC");
+
+        // assert
+        assertNotNull(res);
+        List<String> names = res.getFollowers()
+                .stream()
+                .map(u -> u.getName())
+                .toList();
+
+        assertIterableEquals(names.stream().sorted(Comparator.reverseOrder()).toList(), names);
+    }
+
+    @Test
+    @DisplayName("Cuándo el orden enviado no es ascendente ni descendente, debe arrojar una excepción BadRequestException")
+    public void whenOrderSendedIsNotAscAndIsNotDescShouldThrowsBadRequestException() {
+        // arrange
+        int userId = 2;
+
+        // act
+        // assert
+        assertThrows(BadRequestException.class, () -> userService.findAllFollowersByUser(userId, "INVALID_ORDER"));
+    }
 
 }
