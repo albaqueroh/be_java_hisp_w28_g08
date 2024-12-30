3a14e1a07e47876e214f427871ca5d7057a0d45e
albaqueroh <andres.baquero6@gmail.com>
2024-12-30 10:54:27 -0500

    fix: se corrigen validaciones para CreatePromoPost

--- src/main/java/com/mercadolibre/sprint1/dto/ProductDto.java
+++ src/main/java/com/mercadolibre/sprint1/dto/ProductDto.java
@@ -12,33 +12,33 @@ import lombok.NoArgsConstructor;
 public class ProductDto {
 
 	@JsonProperty("product_id")
-	@NotNull(message = "La id no puede estar vacía.")
+	@NotNull(message = "El id no puede estar vacío.")
 	@Positive(message = "El id debe ser mayor a cero.")
 	private int id;
 
 	@JsonProperty("product_name")
-	@NotEmpty(message = "El campo no puede estar vacío.")
+	@NotEmpty(message = "El nombre no puede estar vacío.")
 	@Size(max = 40, message = "La longitud no puede superar los 40 caracteres.")
-	@Pattern(regexp = "^[A-Za-z0-9 ]*$", message = "El campo no puede poseer caracteres especiales.")
+	@Pattern(regexp = "^[A-Za-z0-9 ]*$", message = "El nombre no puede poseer caracteres especiales.")
 	private String name;
 
-	@NotEmpty
+	@NotEmpty(message = "El tipo no puede estar vacío.")
 	@Size(max = 15, message = "La longitud no puede superar los 15 caracteres.")
-	@Pattern(regexp = "^[A-Za-z0-9 ]*$", message = "El campo no puede poseer caracteres especiales.")
+	@Pattern(regexp = "^[A-Za-z0-9 ]*$", message = "El tipo no puede poseer caracteres especiales.")
 	private String type;
 
-	@NotEmpty
+	@NotEmpty(message = "La marca no puede estar vacía.")
 	@Size(max = 25, message = "La longitud no puede superar los 25 caracteres.")
-	@Pattern(regexp = "^[A-Za-z0-9 ]*$", message = "El campo no puede poseer caracteres especiales.")
+	@Pattern(regexp = "^[A-Za-z0-9 ]*$", message = "La marca no puede poseer caracteres especiales.")
 	private String brand;
 
-	@NotEmpty
+	@NotEmpty(message = "El color no puede estar vacío.")
 	@Size(max = 15, message = "La longitud no puede superar los 15 caracteres.")
-	@Pattern(regexp = "^[A-Za-z0-9 ]*$", message = "El campo no puede poseer caracteres especiales.")
+	@Pattern(regexp = "^[A-Za-z0-9 ]*$", message = "El color no puede poseer caracteres especiales.")
 	private String color;
 
 	@Size(max = 80, message = "La longitud no puede superar los 80 caracteres.")
-	@Pattern(regexp = "^[A-Za-z0-9 ]*$", message = "El campo no puede poseer caracteres especiales.")
+	@Pattern(regexp = "^[A-Za-z0-9 ]*$", message = "Las notas no pueden poseer caracteres especiales.")
 	private String notes;
 
 }
