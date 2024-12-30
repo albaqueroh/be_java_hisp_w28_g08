b85693f9bc44635a4b00b34e18f34470ac3b28a9
andresuarique <andresuarique@gmail.com>
2024-12-30 15:43:18 -0500

    test: metodos Post Reporitory

--- src/test/java/com/mercadolibre/sprint1/repository/unit/PostRepositoryTest.java
+++ src/test/java/com/mercadolibre/sprint1/repository/unit/PostRepositoryTest.java
@@ -1,7 +1,5 @@
 package com.mercadolibre.sprint1.repository.unit;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -9,6 +7,9 @@ import org.junit.jupiter.api.Test;
 import com.mercadolibre.sprint1.entity.Post;
 import com.mercadolibre.sprint1.repository.IRepository;
 import com.mercadolibre.sprint1.repository.impl.PostRepositoryImpl;
+import util.TestUtilGenerator;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PostRepositoryTest {
 
@@ -24,6 +25,78 @@ public class PostRepositoryTest {
         
         // assert
         assertNotEquals(notSize, posts.size());
+    }
+
+    @Test
+    void testFindAll(){
+        // arrange
+
+        // act
+        List<Post> posts = postRepository.findAll();
+
+        // assert
+        assertFalse(posts.isEmpty());
+    }
+
+    @Test
+    void testSave(){
+        // arrange
+        Post newPost = TestUtilGenerator.generatePosts().get(0);
+        // act
+        Post savedPost = postRepository.save(newPost);
+
+        // assert
+        assertNotNull(savedPost.getId());
+        assertEquals(newPost.getUserId(),savedPost.getUserId());
+        assertEquals(newPost.getDate(),savedPost.getDate());
+        assertEquals(newPost.getCategory(),savedPost.getCategory());
+        assertEquals(newPost.getPrice(),savedPost.getPrice());
+        assertEquals(newPost.isHasPromo(),savedPost.isHasPromo());
+        assertEquals(newPost.getDiscount(),savedPost.getDiscount());
+        assertEquals(newPost.getProduct(),savedPost.getProduct());
+    }
+    @Test
+    void testDeleteByIdTrue(){
+        // arrange
+        int id = 1;
+        // act
+        boolean result = postRepository.delete(id);
+
+        // assert
+        assertTrue(result);
+    }
+    @Test
+    void testDeleteByIdFalse(){
+        // arrange
+        int id = 0;
+        // act
+        boolean result = postRepository.delete(id);
+
+        // assert
+        assertFalse(result);
+    }
+    @Test
+    void testDeleteByEntityTrue(){
+        // arrange
+        Post post = TestUtilGenerator.generatePosts().get(0);
+
+        // act
+        postRepository.save(post);
+        boolean result = postRepository.delete(post);
+
+        // assert
+        assertTrue(result);
+    }
+    @Test
+    void testDeleteByEntityFalse(){
+        // arrange
+        Post post = TestUtilGenerator.generatePosts().get(0);
+
+        // act
+        boolean result = postRepository.delete(post);
+
+        // assert
+        assertFalse(result);
     }
 
 }
