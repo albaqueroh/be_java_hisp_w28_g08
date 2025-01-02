7ed24e5770eb0e30f72259e4194564d627ff097e
albaqueroh <andres.baquero6@gmail.com>
2025-01-02 15:46:34 -0500

    fix: corregido importaciones de clases test

--- src/test/java/com/mercadolibre/sprint1/service/unit/ProductServiceTest.java
+++ src/test/java/com/mercadolibre/sprint1/service/unit/ProductServiceTest.java
@@ -1,23 +1,12 @@
 package com.mercadolibre.sprint1.service.unit;
 
-
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.mercadolibre.sprint1.dto.request.NewPostDto;
-import com.mercadolibre.sprint1.entity.UserFollower;
-import com.mercadolibre.sprint1.exception.BadRequestException;
-import com.mercadolibre.sprint1.repository.IRepository;
-import com.mercadolibre.sprint1.service.IUserService;
-import org.junit.jupiter.api.Assertions;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import com.mercadolibre.sprint1.dto.response.FollowersCountDto;
-import com.mercadolibre.sprint1.repository.impl.PostRepositoryImpl;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,22 +14,22 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.mercadolibre.sprint1.dto.request.NewPostDto;
 import com.mercadolibre.sprint1.dto.response.CountProductsPromoDto;
 import com.mercadolibre.sprint1.entity.Post;
-
-import com.mercadolibre.sprint1.service.impl.ProductServiceImpl;
-import util.TestUtilGenerator;
-
 import com.mercadolibre.sprint1.entity.User;
 import com.mercadolibre.sprint1.entity.UserFollower;
+import com.mercadolibre.sprint1.exception.BadRequestException;
 import com.mercadolibre.sprint1.repository.IRepository;
+import com.mercadolibre.sprint1.repository.impl.PostRepositoryImpl;
 import com.mercadolibre.sprint1.service.IUserService;
 import com.mercadolibre.sprint1.service.impl.ProductServiceImpl;
+
 import util.TestUtilGenerator;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
@@ -64,41 +53,41 @@ public class ProductServiceTest {
 
     @Test
     @DisplayName("us-005 Guardado de post")
-    public void whenPostCreatedShouldReturnPost(){
-        //Arrange
+    public void whenPostCreatedShouldReturnPost() {
+        // Arrange
         NewPostDto input = om.convertValue(TestUtilGenerator.generateNoPromoPost(), NewPostDto.class);
         Post inputModel = om.convertValue(input, Post.class);
         String expected = "todo OK";
 
         when(postRepository.save(inputModel)).thenReturn(inputModel);
 
-        //Act & Assert
+        // Act & Assert
         Assertions.assertEquals(expected, productService.newPost(input));
     }
 
     @Test
     @DisplayName("us-005 Error al guardar un post")
-    public void whenBadPostCreatedShouldReturnError(){
-        //Arrange
+    public void whenBadPostCreatedShouldReturnError() {
+        // Arrange
         NewPostDto input = om.convertValue(TestUtilGenerator.generateNoPromoPost(), NewPostDto.class);
         input.setUserId(null);
 
-        //Act & Assert
+        // Act & Assert
         Assertions.assertThrows(BadRequestException.class, () -> productService.newPost(input));
     }
 
     @Test
     @DisplayName("us-0010 Guardado de post con promo validos")
     public void whenPostValidShouldReturnConfirmationMessage() {
-        //Arrange
+        // Arrange
         when(postRepository.save(TestUtilGenerator.CreatePromoPost())).thenReturn(TestUtilGenerator.CreatePromoPost());
 
-        //act
+        // act
         String outputPostPromoSave = productService.createPromoPost(TestUtilGenerator.createPostPromoDto());
 
-        //assert
+        // assert
         verify(postRepository, atLeastOnce()).save(TestUtilGenerator.CreatePromoPost());
-        assertEquals("Post guardado",outputPostPromoSave);
+        assertEquals("Post guardado", outputPostPromoSave);
     }
 
     @Test
--- src/test/java/com/mercadolibre/sprint1/service/unit/UserFollowerServiceTest.java
+++ src/test/java/com/mercadolibre/sprint1/service/unit/UserFollowerServiceTest.java
@@ -1,37 +1,31 @@
 package com.mercadolibre.sprint1.service.unit;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import com.mercadolibre.sprint1.dto.response.UnfollowResponseDto;
-import com.mercadolibre.sprint1.exception.NotFoundException;
-import com.mercadolibre.sprint1.service.IUserFollowerService;
-import org.junit.jupiter.api.Assertions;
-
-import com.mercadolibre.sprint1.dto.response.FollowersCountDto;
-import com.mercadolibre.sprint1.repository.impl.UserFollowerRepositoryImpl;
-import com.mercadolibre.sprint1.repository.impl.UserRepositoryImpl;
-import com.mercadolibre.sprint1.service.impl.UserFollowerServiceImpl;
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import com.mercadolibre.sprint1.entity.User;
+import com.mercadolibre.sprint1.dto.response.FollowersCountDto;
+import com.mercadolibre.sprint1.dto.response.UnfollowResponseDto;
 import com.mercadolibre.sprint1.entity.UserFollower;
-import com.mercadolibre.sprint1.repository.IRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.mercadolibre.sprint1.exception.NotFoundException;
+import com.mercadolibre.sprint1.repository.impl.UserFollowerRepositoryImpl;
+import com.mercadolibre.sprint1.repository.impl.UserRepositoryImpl;
+import com.mercadolibre.sprint1.service.impl.UserFollowerServiceImpl;
+
 import util.TestUtilGenerator;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
-
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 public class UserFollowerServiceTest {
@@ -47,54 +41,57 @@ public class UserFollowerServiceTest {
 
     @Test
     @DisplayName("US0007 - Cuando se esta siguiendo al vendedor debe dejar de seguirlo")
-    void whenSellerIsFollowedShouldUnfollow(){
+    void whenSellerIsFollowedShouldUnfollow() {
         // arrange
         int userId = 1;
         int sellerId = 2;
-        UnfollowResponseDto expectedResponse = new UnfollowResponseDto("Se ha dejado de seguir al usuario "+sellerId);
-        UserFollower userFollower = new UserFollower(userId,sellerId);
+        UnfollowResponseDto expectedResponse = new UnfollowResponseDto("Se ha dejado de seguir al usuario " + sellerId);
+        UserFollower userFollower = new UserFollower(userId, sellerId);
 
         // act
-        when(userFollowerRepository.findByFollowerIdAndFollowedId(userId,sellerId)).thenReturn(Optional.of(userFollower));
+        when(userFollowerRepository.findByFollowerIdAndFollowedId(userId, sellerId))
+                .thenReturn(Optional.of(userFollower));
         when(userFollowerRepository.delete(userFollower)).thenReturn(true);
 
-        UnfollowResponseDto result = userFollowerService.unfollow(userId,sellerId);
+        UnfollowResponseDto result = userFollowerService.unfollow(userId, sellerId);
 
         // assert
-        verify(userFollowerRepository, atLeast(1)).findByFollowerIdAndFollowedId(userId,sellerId);
+        verify(userFollowerRepository, atLeast(1)).findByFollowerIdAndFollowedId(userId, sellerId);
         verify(userFollowerRepository, atLeast(1)).delete(userFollower);
         assertThat(expectedResponse).isEqualTo(result);
     }
-  
+
     @Test
     @DisplayName("US0007 - Cuando no se esta siguiendo al vendedor debe arrojar una excepcion NotFoundException")
-    void whenSellerIsNotFollowedShouldThrowsNotFoundException(){
+    void whenSellerIsNotFollowedShouldThrowsNotFoundException() {
         // arrange
         int userId = 1;
         int sellerId = 100;
 
         // act
-        when(userFollowerRepository.findByFollowerIdAndFollowedId(userId,sellerId)).thenReturn(Optional.empty());
+        when(userFollowerRepository.findByFollowerIdAndFollowedId(userId, sellerId)).thenReturn(Optional.empty());
 
         // assert
-        assertThrows(NotFoundException.class, ()->{userFollowerService.unfollow(userId,sellerId);});
+        assertThrows(NotFoundException.class, () -> {
+            userFollowerService.unfollow(userId, sellerId);
+        });
     }
-  
+
     @Test
     @DisplayName("us-0002 Devuelve el cálculo correcto del total de la cantidad de seguidores que posee un usuario.")
     public void whenUserExistsShouldReturnCount() {
-        //Arrange
+        // Arrange
         int idUser = 1;
         when(userFollowerRepository.findAll()).thenReturn(TestUtilGenerator.generateFollowers());
         when(userRepository.findAll()).thenReturn(TestUtilGenerator.generateUsers());
 
-        //act
+        // act
         FollowersCountDto outputFollowerCount = userFollowerService.followersCount(idUser);
 
-        //assert
+        // assert
         verify(userFollowerRepository, atLeastOnce()).findAll();
         verify(userRepository, atLeastOnce()).findAll();
-        assertEquals(TestUtilGenerator.expectedResponseFollowerCount(),outputFollowerCount);
+        assertEquals(TestUtilGenerator.expectedResponseFollowerCount(), outputFollowerCount);
 
     }
 
