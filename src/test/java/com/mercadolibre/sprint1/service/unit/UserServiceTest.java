8f6acc94c893d57d1b768d6f3f94ed243c8dddcc
Cristhian <crispito2008@hotmail.com>
2025-01-02 16:03:32 -0500

    feat: Se realiza test cuando un usuario es vendedor y exite, y cuando el usuario a seguir no existe

--- src/test/java/com/mercadolibre/sprint1/service/unit/UserServiceTest.java
+++ src/test/java/com/mercadolibre/sprint1/service/unit/UserServiceTest.java
@@ -1,9 +1,11 @@
 package com.mercadolibre.sprint1.service.unit;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+import com.mercadolibre.sprint1.entity.UserFollower;
+import com.mercadolibre.sprint1.exception.NotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -41,6 +43,38 @@ public class UserServiceTest {
     public void initialize() {
         when(userFollowerRepository.findAll()).thenReturn(TestUtilGenerator.generateFollowers());
         when(userRepository.findAll()).thenReturn(TestUtilGenerator.generateUsers());
+    }
+
+    @Test
+    @DisplayName("US0001 - Cuándo el usuario existe y es vendedor debe retornar true")
+    public void followUserWhenUserExistsAndIsSellerShouldReturnTrue(){
+        // Arrange
+        int userId = 1;
+        int userIdToFollow = 2;
+        UserFollower userFollower = new UserFollower(userIdToFollow, userId);
+        when(userRepository.findAll()).thenReturn(TestUtilGenerator.generateUsers());
+        when(userFollowerRepository.save(userFollower)).thenReturn(userFollower);
+
+        // Act
+        boolean result = userService.followUser(userId, userIdToFollow);
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("US0001 - Cuándo el usuario a seguir no exite debe retornar una NotFoundException")
+    public void followUserWhenUserToFollowDoesNotExistShouldThrowNotFoundException(){
+        // Arrange
+        int userId = 1;
+        int userIdToFollow = 999;
+        when(userRepository.findAll()).thenReturn(TestUtilGenerator.generateUsers());
+        // Act
+        Exception exception = assertThrows(NotFoundException.class, () -> {
+            userService.followUser(userId, userIdToFollow);
+        });
+        // Assert
+        assertEquals("No existe el usuario " + userIdToFollow, exception.getMessage());
     }
 
     @Test
