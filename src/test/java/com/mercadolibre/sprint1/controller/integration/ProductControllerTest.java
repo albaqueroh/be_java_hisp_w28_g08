7a6b77aa0826ffb22681cf9fe00c0a7ddd1983a1
Andrés Morera <felipemorera15@gmail.com>
2025-01-03 10:32:05 -0500

    feat: adición integration test para el guradado de promo post

--- src/test/java/com/mercadolibre/sprint1/controller/integration/ProductControllerTest.java
+++ src/test/java/com/mercadolibre/sprint1/controller/integration/ProductControllerTest.java
@@ -2,6 +2,7 @@ package com.mercadolibre.sprint1.controller.integration;
 
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.mercadolibre.sprint1.dto.request.CreatePromoPostDto;
 import com.mercadolibre.sprint1.dto.request.NewPostDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,8 +31,10 @@ public class ProductControllerTest {
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
     @Test
-    @DisplayName("US005 - Creación de usuario correctamente")
+    @DisplayName("US005 - Creación de post correctamente")
     public void whenPostCreatedShouldReturnTodoOk() throws Exception{
+
+        //Arrange
         NewPostDto paramPost = om.convertValue(TestUtilGenerator.generateNoPromoPost(), NewPostDto.class);
         String expected = "todo OK";
 
@@ -39,6 +42,7 @@ public class ProductControllerTest {
                 configure(SerializationFeature.WRAP_ROOT_VALUE, false).writer().withDefaultPrettyPrinter();
         String payloadJson = writer.writeValueAsString(paramPost);
 
+        //Act & assert
         mockMvc.perform(post("/products/post").contentType(MediaType.APPLICATION_JSON)
                     .content(payloadJson))
                 .andDo(print())
@@ -47,8 +51,10 @@ public class ProductControllerTest {
     }
 
     @Test
-    @DisplayName("US005 - Creación de usuario incorrectamente")
+    @DisplayName("US005 - Creación de post incorrectamente")
     public void whenBadPostCreatedShouldReturnError() throws Exception{
+
+        //Arrange
         NewPostDto paramPost = om.convertValue(TestUtilGenerator.generateNoPromoPost(), NewPostDto.class);
         paramPost.setUserId(null);
         String expected = "El  id no puede estar vacío.";
@@ -57,11 +63,53 @@ public class ProductControllerTest {
                 configure(SerializationFeature.WRAP_ROOT_VALUE, false).writer().withDefaultPrettyPrinter();
         String payloadJson = writer.writeValueAsString(paramPost);
 
+        //Act & assert
         mockMvc.perform(post("/products/post").contentType(MediaType.APPLICATION_JSON)
                         .content(payloadJson))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
                 .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.errors[0].message").value(expected));
+    }
+
+    @Test
+    @DisplayName("US0010 - Creación de post promo correctamente")
+    public void whenPostPromoCreatedShouldReturnOk() throws Exception{
+
+        //Arrange
+        CreatePromoPostDto promoPost = TestUtilGenerator.createPostPromoDto();
+        String expected = "Post guardado";
+
+        ObjectWriter writer = new ObjectMapper().registerModule(new JavaTimeModule()).
+                configure(SerializationFeature.WRAP_ROOT_VALUE, false).writer().withDefaultPrettyPrinter();
+        String payloadJson = writer.writeValueAsString(promoPost);
+
+        //Act & assert
+        mockMvc.perform(post("/products/promo-post").contentType(MediaType.APPLICATION_JSON)
+                        .content(payloadJson))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(expected));
+    }
+
+    @Test
+    @DisplayName("US0010 - Creación de post promo incorrectamente")
+    public void whenBadPostPromoCreatedShouldReturnError() throws Exception{
+
+        //Arrange
+        CreatePromoPostDto promoPost = TestUtilGenerator.createPostPromoDto();
+        promoPost.setUserId(null);
+        String expected = "El user id no debe ser nulo";
+
+        ObjectWriter writer = new ObjectMapper().registerModule(new JavaTimeModule()).
+                configure(SerializationFeature.WRAP_ROOT_VALUE, false).writer().withDefaultPrettyPrinter();
+        String payloadJson = writer.writeValueAsString(promoPost);
+
+        //Act & assert
+        mockMvc.perform(post("/products/promo-post").contentType(MediaType.APPLICATION_JSON)
+                        .content(payloadJson))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.errors[0].message").value(expected));
     }
 
