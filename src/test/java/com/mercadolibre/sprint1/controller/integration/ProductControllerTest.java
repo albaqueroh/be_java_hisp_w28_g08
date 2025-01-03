be98c6cb23cce5d43da13d4a7f3f6899099fb552
albaqueroh <andres.baquero6@gmail.com>
2025-01-03 09:11:22 -0500

    feat: se agrega test de integración para US0001

--- src/test/java/com/mercadolibre/sprint1/controller/integration/ProductControllerTest.java
+++ src/test/java/com/mercadolibre/sprint1/controller/integration/ProductControllerTest.java
@@ -1,10 +1,24 @@
 package com.mercadolibre.sprint1.controller.integration;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import com.mercadolibre.sprint1.dto.response.MostFollowedUserDto;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ProductControllerTest {
 
     @Autowired
